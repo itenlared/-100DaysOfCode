7120f0272ea09ef912ae4298d01a5cd682cf5ed2
itenlared <42077452+itenlared@users.noreply.github.com>
2018-08-04 21:10:04 -0300

    Add files via upload

---
+++ dia2/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Yo Lo Vendo</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<?php require('header.php') ?>
+<div class="Hola">Hola Mundo</div>
+</body>
+</html>
